29c907f5aa9a10aaf6329b316f6248f7a161873c
MahBoiTranslator <mah.boi.translator@gmail.com>
2021-03-17 16:55:00 +0300

    Выгрузка файлов

---
+++ Build.bat
@@ -0,0 +1,4 @@
+mkdir Translation
+@start Tools\MakeBig.exe -f -o:Translation\GenEvo_B0.21_English.big Source\csf
+mkdir Translation\Launcher
+xcopy /h /y /c /r /s Source\Launcher Translation\Launcher

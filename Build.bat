1c4eabc349715b63091ba799f6f12ea1b855f8ab
MahBoiTranslator <mah.boi.translator@gmail.com>
2021-06-11 01:07:18 +0300

    Добавлен readme в файлы сборки

--- Build.bat
+++ Build.bat
@@ -1,4 +1,5 @@
 @mkdir Translation
 @start Tools\MakeBig.exe -f -o:Translation\GenEvo_B0.21_English.big Source\csf
 @mkdir Translation\Launcher
+@copy Source\readme.txt Translation\readme.txt
 @xcopy /h /y /c /r /s Source\Launcher Translation\Launcher

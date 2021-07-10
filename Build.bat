8ea3e1c98067f222d0d34f1ba069177167e9fe01
MahBoiTranslator <mah.boi.translator@gmail.com>
2021-07-10 12:16:09 +0300

    Улучшение перевода и удобства использования батника

--- Build.bat
+++ Build.bat
@@ -1,5 +1,6 @@
-@mkdir Translation
-@start Tools\MakeBig.exe -f -o:Translation\GenEvo_B0.21_English.big Source\csf
-@mkdir Translation\Launcher
-@copy Source\readme.txt Translation\readme.txt
-@xcopy /h /y /c /r /s Source\Launcher Translation\Launcher
+@echo off
+mkdir Translation
+start Tools\MakeBig.exe -f -o:Translation\GenEvo_B0.21_English.big Source\csf
+mkdir Translation\Launcher
+copy Source\readme.txt Translation\readme.txt
+xcopy /h /y /c /r /s Source\Launcher Translation\Launcher

3ec762f77ca9c391e6e3efd4c1ea7c04ee87dd39
MahBoiTranslator <mah.boi.translator@gmail.com>
2021-07-11 00:46:33 +0300

    Перемещена папка с картами, изменён батник

--- Build.bat
+++ Build.bat
@@ -1,6 +1,14 @@
 @echo off
+
+: Сборка .big файла перевода
 mkdir Translation
 start Tools\MakeBig.exe -f -o:Translation\GenEvo_B0.21_English.big Source\csf
+
+: "Сборка" клиента
 mkdir Translation\Launcher
 copy Source\readme.txt Translation\readme.txt
 xcopy /h /y /c /r /s Source\Launcher Translation\Launcher
+
+: "Сборка карт
+mkdir Translation\Maps
+xcopy /h /y /c /r /s "Source\Maps with text\" Translation\Maps

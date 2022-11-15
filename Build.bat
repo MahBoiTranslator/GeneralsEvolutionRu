b58271621d13ee52bdde0fc8ad16808d5b8a91ce
MahBoiTranslator <mah.boi.translator@gmail.com>
2022-11-16 00:09:23 +0300

    Перевод лаунчера и подготовка к переводу мода и карт

--- Build.bat
+++ Build.bat
@@ -7,7 +7,7 @@ start Tools\MakeBig.exe -f -o:Translation\GenEvo_B0.3_English.big Source\csf
 : "Сборка" клиента
 mkdir Translation\Launcher
 copy Source\readme.txt Translation\readme.txt
-xcopy /h /y /c /r /s Source\Launcher Translation\Launcher
+xcopy /h /y /c /r /s Source\Launcher\* Translation\Launcher\*
 
 : "Сборка" карт
 mkdir Translation\Maps

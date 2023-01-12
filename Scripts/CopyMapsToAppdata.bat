b32dc0b55410d45a7d31246be48714ad4f377d3c
MahBoiTranslator <mah.boi.translator@gmail.com>
2023-01-13 02:33:08 +0300

    Правки в скриптовую систему

--- Scripts/CopyMapsToAppdata.bat
+++ Scripts/CopyMapsToAppdata.bat
@@ -1,7 +1,4 @@
 @echo off
 
 : Копирование карт из текущего репозитория в %appdata%
-xcopy /h /y /c /r /s ..\Source\Maps\* %appdata%\"Red Alert 3"\Maps\*
-del         %appdata%\"Red Alert 3"\Maps\*.str %appdata%\"Red Alert 3"\Maps\*.md
-del /a:h    %appdata%\"Red Alert 3"\Maps\.git
-rmdir /s /q %appdata%\"Red Alert 3"\Maps\.github
+xcopy /h /y /c /r /s ..\Source\Maps\[GenEvo]* %appdata%\"Red Alert 3"\Maps\[GenEvo]*
